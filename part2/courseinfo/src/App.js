b48cd733e4981f422674b3e290e220985aa6c91b
askar200018 <imangali950@gmail.com>
2022-11-23 19:49:44 +0600

    2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,32 +1,4 @@
-const Header = ({ title }) => <h1>{title}</h1>;
-
-const Total = ({ sum }) => <h4>Number of exercises {sum}</h4>;
-
-const Part = ({ part }) => (
-  <li>
-    {part.name} {part.exercises}
-  </li>
-);
-
-const Content = ({ parts }) => (
-  <ul>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </ul>
-);
-
-const Course = ({ course }) => {
-  const total = course.parts.reduce((acc, curr) => acc + curr.exercises, 0);
-
-  return (
-    <div>
-      <Header title={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={total} />
-    </div>
-  );
-};
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
