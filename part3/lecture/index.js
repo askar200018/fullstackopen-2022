b61830ed98d14ab6dec6c58491714a9b3de0d4f5
askar200018 <imangali950@gmail.com>
2022-12-03 15:33:39 +0600

    part3: c Saving data to MongoDB (connect mongo to backend)

--- part3/lecture/index.js
+++ part3/lecture/index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Note = require('./models/note');
+
 const app = express();
 
 app.use(express.json());
@@ -42,7 +46,9 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 const generateId = () => {
@@ -59,26 +65,21 @@ app.post('/api/notes', (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
-  notes = notes.concat(note);
+  });
 
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = +request.params.id;
-  const note = notes.find((n) => n.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -94,7 +95,7 @@ const unknownEndpoint = (request, response) => {
 };
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
