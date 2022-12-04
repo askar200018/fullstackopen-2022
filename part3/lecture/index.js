05e05073c7841473321a7b19dac7ab360339eab3
askar200018 <imangali950@gmail.com>
2022-12-05 01:07:09 +0600

    part3: d Validation and ESLint (lint)

--- part3/lecture/index.js
+++ part3/lecture/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const Note = require('./models/note');
 
 const app = express();
@@ -20,27 +19,6 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2022-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2022-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2022-05-30T19:20:14.298Z',
-    important: true,
-  },
-];
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
@@ -50,12 +28,6 @@ app.get('/api/notes', (request, response) => {
     response.json(notes);
   });
 });
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
-  return maxId + 1;
-};
 
 app.post('/api/notes', (request, response, next) => {
   const body = request.body;
@@ -93,7 +65,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end();
     })
     .catch((error) => next(error));
