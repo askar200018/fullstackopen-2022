42bd65930d6445db4ff68d483fe772567eb55a2b
askar200018 <imangali950@gmail.com>
2022-12-05 01:11:40 +0600

    3.22: Lint configuration

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,7 +6,7 @@ const Person = require('./models/person');
 
 const app = express();
 
-morgan.token('body', function (req, res) {
+morgan.token('body', function (req) {
   return JSON.stringify(req.body);
 });
 
@@ -17,38 +17,11 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons);
   });
 });
-
-const generateId = () => {
-  return Math.floor(Math.random() * 100000);
-};
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body;
