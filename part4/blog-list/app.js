8bfbcf8e2a714af55febc5263b41766716067500
askar200018 <imangali950@gmail.com>
2022-12-06 01:57:17 +0600

    4.2 Blog list, step2

---
+++ part4/blog-list/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const app = express();
+
+const mongoUrl = config.MONGODB_URI;
+
+logger.info('connecting to', mongoUrl);
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
