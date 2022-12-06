436562b4e2279d3a6d2864aa92f36758af140434
askar200018 <imangali950@gmail.com>
2022-12-06 19:51:29 +0600

    4.6*, 4.7*: (rewrote to lodash)

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -24,49 +26,39 @@ const favoriteBlog = (blogs) => {
 };
 
 const mostBlogs = (blogs) => {
-  const authors = blogs.reduce((acc, { author }) => {
-    if (Object.prototype.hasOwnProperty.call(acc, author)) {
-      acc[author] += 1;
-    } else {
-      acc[author] = 1;
-    }
-    return acc;
-  }, {});
+  if (blogs.length === 0) {
+    return null;
+  }
+  const authors = _.chain(blogs)
+    .groupBy('author')
+    .map((value, key) => ({
+      author: key,
+      blogs: value.length,
+    }))
+    .value();
 
-  const withMostBlogs = Object.entries(authors).reduce((acc, [key, value]) => {
-    if (acc === null || value > acc.blogs) {
-      return {
-        author: key,
-        blogs: value,
-      };
-    }
-
-    return acc;
-  }, null);
+  const withMostBlogs = authors.reduce((acc, curr) => {
+    return curr.blogs > acc.blogs ? curr : acc;
+  });
 
   return withMostBlogs;
 };
 
 const mostLikes = (blogs) => {
-  const authors = blogs.reduce((acc, { author, likes }) => {
-    if (Object.prototype.hasOwnProperty.call(acc, author)) {
-      acc[author] += likes;
-    } else {
-      acc[author] = likes;
-    }
-    return acc;
-  }, {});
+  if (blogs.length === 0) {
+    return null;
+  }
+  const authors = _.chain(blogs)
+    .groupBy('author')
+    .map((value, key) => ({
+      author: key,
+      likes: value.reduce((acc, curr) => acc + curr.likes, 0),
+    }))
+    .value();
 
-  const withMostLikes = Object.entries(authors).reduce((acc, [key, value]) => {
-    if (acc === null || value > acc.likes) {
-      return {
-        author: key,
-        likes: value,
-      };
-    }
-
-    return acc;
-  }, null);
+  const withMostLikes = authors.reduce((acc, curr) => {
+    return curr.likes > acc.likes ? curr : acc;
+  });
 
   return withMostLikes;
 };
