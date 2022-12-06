65d0d08b0f0681b18c52d44428a89f49c317aa3f
askar200018 <imangali950@gmail.com>
2022-12-06 19:11:55 +0600

    4.5*: helper functions and unit tests, step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -8,7 +8,23 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  const blog = blogs.reduce((acc, curr) => {
+    return curr.likes > acc.likes ? curr : acc;
+  });
+
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
