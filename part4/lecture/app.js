e30e38a2fddee177fd5d20714c2b71ea771a5ec2
askar200018 <imangali950@gmail.com>
2022-12-13 02:32:25 +0600

    part4: c User administration (created users)

--- part4/lecture/app.js
+++ part4/lecture/app.js
@@ -3,6 +3,7 @@ require('express-async-errors');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const notesRouter = require('./controllers/notes');
+const usersRouter = require('./controllers/users');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
@@ -33,6 +34,7 @@ app.get('/', (request, response) => {
 });
 
 app.use('/api/notes', notesRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
