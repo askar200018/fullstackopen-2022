653d6c7dbdd3ebe2cea2a0578846546f99328e02
askar200018 <imangali950@gmail.com>
2022-12-26 17:04:54 +0600

    4.15: bloglist expansion, step3

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const app = express();
@@ -23,5 +24,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
