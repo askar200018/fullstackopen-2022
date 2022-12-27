6301225c859ab116baecd412256ac85e891071bb
askar200018 <imangali950@gmail.com>
2022-12-27 16:36:44 +0600

    4.21*: bloglist expansion, step9

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -38,7 +38,20 @@ blogsRouter.post('/', async (request, response) => {
 
 blogsRouter.delete('/:id', async (request, response) => {
   try {
-    await Blog.findByIdAndRemove(request.params.id);
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({
+        error: 'token missing or invalid',
+      });
+    }
+    const blog = await Blog.findById(request.params.id);
+    
+    if (blog.user.toString() !== decodedToken.id.toString()) {
+      return response.status(400).json({
+        error: 'wrong owner id',
+      });
+    }
+    await blog.remove();
     response.status(204).end();
   } catch (exception) {
     response.status(400).send();
